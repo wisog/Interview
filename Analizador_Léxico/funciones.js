30590eb4bedbdbbf2876e666e1b25ee437ece01e
IngCampos <15151130@mail.ita.mx>
2020-01-06 11:48:33 -0600

    Analizador_Léxico Solucion final
    Se mejoró para que funcione con coordenadas repetidas.
    Y asi cumpliendo con todos los requerimientos y ademas funcione para coordenadas negativas y repetidas

--- Analizador_Léxico/funciones.js
+++ Analizador_Léxico/funciones.js
@@ -8,6 +8,13 @@ function Splitmultiple(texto_completo, array_textos) {
         x = texto_completo.split(array_textos[array_texto]);
         textos_resultante.push(x[0]);
         texto_completo = x[1];
+        if(x.length >2){
+            // En caso de haber coordenadas repetidas, esto genera mas de 2 fragmentos(invalidando la funcion).
+            // Aqui se unen del 2do fragmenro hasta N fragmentos para que la interaccion siga normal.
+            for(var i =2; i<=(x.length-1);i++){
+                texto_completo += array_textos[array_texto]+x[i];
+              }
+        }
         if ((array_textos.length - 1) == array_texto) {
             // la resultante ultima se toma asi al no tener esta una coordenada
             // y tambien por que el primer valor de textpos_resultantes no es valor valido para una coordenada
